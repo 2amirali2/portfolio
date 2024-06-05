faa4913fb05143d5184215023d7861e29911f7ba
aamigo <aamigodeveloper@gmail.com>
2024-06-05 18:32:15 +0330

    fix footer bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { FaLocationArrow } from "react-icons/fa6"
 
 import { socialMedia } from "@/data"
@@ -24,20 +25,20 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:contact@jsmastery.pro">
+        {/* <a href="mailto:contact@jsmastery.pro">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
-        </a>
+        </a> */}
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Amirali khubru
         </p>
 
-        <div className="flex items-center md:gap-3 gap-6">
+        <div className="flex items-center md:gap-3 gap-6 mt-4">
           {socialMedia.map((info) => (
             <div
               key={info.id}
